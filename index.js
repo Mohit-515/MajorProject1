1630bec25324187019c97e422f54db6ec70dd83b
mohits Projects <mohit5150515@gmail.com>
2020-06-02 22:04:02 +0530

    commited till sign up

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express=require('express');
+const cookieParser=require('cookie-parser');
 const app = express();
 const port=8000;
 const expressLayouts = require('express-ejs-layouts');
 
+const db=require('./config/mongoose');
+
+app.use(express.urlencoded());
+app.use(cookieParser());
+
 app.use(express.static('./assests'));
 
 app.use(expressLayouts);
+
+//extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 app.use('/',require('./routes'));
 
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,10 @@ const router = express.Router();
 const usersController = require('../controllers/users_Controller');
 router.get('/profile', usersController.profile);
 
+router.get('/sign-up',usersController.signUp);
+router.get('/sign-in',usersController.signIn);
+
+router.post('/create',usersController.create);
+router.post('/create-session', usersController.createSession);
+
 module.exports = router;
