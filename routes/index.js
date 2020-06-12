3e8750b1bb1166ba722e746e91133ff22c648236
mohits Projects <mohit5150515@gmail.com>
2020-06-13 00:18:42 +0530

    database relation with posts comments

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ console.log("router loaded");
 router.get('/', homeController.home);
 router.use('/users',require('./users'));
 router.use('/friends',require('./friends'));
+router.use('/posts', require('./posts'));
 //for any further routes,access from here
 //router.use('/routerName',require('./routerfile'))
 
